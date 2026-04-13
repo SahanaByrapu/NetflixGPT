047af08665f2748628b0b0ecf02fbb4d562c3c74
Sai Sahana Bhargavi Byrapu <101760308+SahanaByrapu@users.noreply.github.com>
2026-04-13 14:07:10 -0700

    created plan for basic netflix layout and created react-app

---
+++ netflix-gpt/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-3xl font-bold text-green-800">
+     Hello Everyone, let's build Netflix GPT
+    </div>
+  );
+}
+
+export default App;
